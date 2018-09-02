14da79d0b714a32981442852637a7109a6ed0cac
Ryan Nett <rnett@calpoly.edu>
2018-09-02 16:20:08 -0700

    fix

--- src/main/kotlin/com/rnett/core/PooledDBConnection.kt
+++ src/main/kotlin/com/rnett/core/PooledDBConnection.kt
@@ -4,9 +4,9 @@ import org.apache.commons.dbcp.BasicDataSource
 
 //TODO JNDI from jetty?
 object PooledDBConnection {
-    fun connect(connectionURL: String) = {
+    fun connect(connectionURL: String): BasicDataSource {
         val ds = BasicDataSource()
         ds.url = connectionURL
-        ds
+        return ds
     }
 }
