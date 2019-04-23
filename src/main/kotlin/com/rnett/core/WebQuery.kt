6ea61f08cfa5a50727dfe489fe1dad9f7be439dc
Ryan Nett <rnett@calpoly.edu>
2019-04-22 20:03:29 -0700

    keyed transforms

--- src/main/kotlin/com/rnett/core/WebQuery.kt
+++ src/main/kotlin/com/rnett/core/WebQuery.kt
@@ -102,6 +102,19 @@ class WebQuery(val concurrency: Int = 100,
         }.awaitAll()
     }
 
+    suspend inline fun <reified T : Any, I, R> makeQueries(inputs: List<Pair<I, String>>, crossinline transform: (I, T) -> R): List<R?> {
+        val launchpad = Launchpad<R?>(concurrency, concurrencyStart, concurrencyStep, coroutineContext)
+
+        return inputs.map { (key, url) ->
+            launchpad {
+                makeJsonQuery<T>(url)?.let { transform(key, it) }
+            }
+        }.awaitAll()
+    }
+
+    suspend inline fun <reified T : Any, I, R> makeQueries(inputs: List<I>, toUrl: (I) -> String, crossinline transform: (I, T) -> R): List<R?> =
+            makeQueries(inputs.associateWith { toUrl(it) }.toList(), transform)
+
     suspend inline fun <reified T : Any> makeJsonQuery(url: String) =
             makeQuery(url)?.let { Gson().fromJson<T>(it) }
 
